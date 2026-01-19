fc8e9ec68fc2f5ffed1e742fdcb1c8e6f6cec12f
yfujita <fujiriko59@gmail.com>
2026-01-19 10:02:17 +0900

    fix: Can not scroll

--- app/src/main/java/io/github/yfujita/herenow/ui/MainScreen.kt
+++ app/src/main/java/io/github/yfujita/herenow/ui/MainScreen.kt
@@ -8,6 +8,8 @@ import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -99,7 +101,8 @@ fun MainScreen(viewModel: MainViewModel) {
             Column(
                 modifier = Modifier
                     .padding(innerPadding)
-                    .fillMaxSize(),
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ) {
